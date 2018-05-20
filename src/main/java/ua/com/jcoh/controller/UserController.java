65d769bcaa758edbe2351196a50f24fe3d56b264
Andriy Androsyuk <andriiandrosiukk@gmail.com>
2018-05-20 14:31:54 +0200

    redirect to main page

--- src/main/java/ua/com/jcoh/controller/UserController.java
+++ src/main/java/ua/com/jcoh/controller/UserController.java
@@ -31,7 +31,7 @@ public class UserController {
     @Autowired
     private UserValidator userValidator;
 
-    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String registration(Model model) {
         model.addAttribute("userForm", new User());
 
